d7436eaeb5b1045545c87c2da773511533db875e
n0vedad <lucas.daemmig@gmx.net>
2024-09-09 00:19:37 +0200

    move privilege check for linux to specific file

--- cmd/tpmk/main.go
+++ cmd/tpmk/main.go
@@ -1,36 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"runtime"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-func checkAdminRights() bool {
-	if runtime.GOOS == "windows" {
-		_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
-		return err == nil
-	}
-	return os.Geteuid() == 0
-}
-
 func main() {
-	if checkAdminRights() {
-		fmt.Println("Warning: This program is running with elevated privileges.")
-		fmt.Println("It is recommended to run it as a regular user and use elevated privileges only when prompted.")
-		fmt.Println("Continue? (yes/no)")
-		var response string
-		fmt.Scanln(&response)
-		if strings.ToLower(response) != "yes" {
-			fmt.Println("Exiting.")
-			os.Exit(1)
-		}
-	}
-
-	// Register the sub-commands under root
 	rootCmd := newRootCommand()
 	rootCmd.AddCommand(
 		newNVCommand(),
